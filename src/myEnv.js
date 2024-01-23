3e49da56cea80bfb7a451b9efe85cf654bd7b807
Piyush Raj <piyushraj707@gmail.com>
2024-01-23 19:26:40 +0530

    changed base_url

--- src/myEnv.js
+++ src/myEnv.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3002"
+// export const BASE_URL = "http://localhost:3002"
 // export const BASE_URL = "https://exp-backend-peach.vercel.app"
-// export const BASE_URL = "https://fresh-backend.vercel.app"
+export const BASE_URL = "https://fresh-backend.vercel.app"
